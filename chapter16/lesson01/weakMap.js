23611e8786b7fb0dcba3fe87efd4bfebc2f8949d
AkitoTsukahara <tsukahara.akito@gmail.com>
2021-02-27 00:06:45 +0900

    edit

--- chapter16/lesson01/weakMap.js
+++ chapter16/lesson01/weakMap.js
@@ -1,3 +1,7 @@
+// Mapと違って、キーとして使われるオブジェクトが生きているうちに、WeakMapがキーの参照を持ちますが、
+// WeakMap以外誰にも使われていなければ、ガベージコレクションによってキーオブジェクトのメモリを解放してしまいます。
+// つまり、キーの参照が弱い参照であり、自動的に削除される可能性があります。
+
 const wm = new WeakMap();
 
 const key1 = {};
@@ -6,6 +10,7 @@ wm.set(key1, 100);
 
 console.log(wm.get(key1));
 
+// 同一のオブジェクトでないと参照できないことを意味する
 console.log(key1 === {});
 
 function saveValueObject(obj, value) {
@@ -24,4 +29,4 @@ const a = {
 const wm1 = new WeakMap();
 wm1.set(a, 100);
 
-console.log()
+console.log(wm1.get(a));
