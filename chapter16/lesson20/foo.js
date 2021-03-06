44a4bde9455a035bd77c821435c54c63aa893f01
AkitoTsukahara <tsukahara.akito@gmail.com>
2021-03-06 21:03:59 +0900

    module

---
+++ chapter16/lesson20/foo.js
@@ -0,0 +1,9 @@
+// foo.js
+// exports.fooFunction = function () {
+//     console.log('Hi');
+// }
+
+export default function fooFunction() {
+    console.log('Hi');
+}
+
---
+++ chapter16/lesson20/module.js
@@ -0,0 +1,18 @@
+// const {
+//     fooFunction,
+// } = require('./foo.js');
+
+// bar.js
+import {
+    fooFunction,
+} from './foo.js';
+
+
+// import {
+//     foo as superFoo,
+//     bar,
+// } from "./foo.js";
+
+
+fooFunction();
+
